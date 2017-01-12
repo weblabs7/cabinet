bd36f708c0a6e4347f49f60a910dd71329fcf315
weblabs7 <weblabs@live.com>
2017-01-12 20:29:57 +0000

    edit update destroy.

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,6 +1,8 @@
 class DocsController < ApplicationController
+    before_action :find_doc, only: [:show, :edit, :update, :destroy]
     
     def index
+        @docs = Doc.all.order('created_at DESC')
     end
     
     def show
@@ -32,6 +34,7 @@ class DocsController < ApplicationController
     private
     
     def find_doc
+         @doc = Doc.find(params[:id])
     end
     
     def doc_params
