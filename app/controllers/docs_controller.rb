47668e3bd8a5dcec219245f3b07a94023c8639f0
weblabs7 <weblabs@live.com>
2017-01-13 14:00:12 +0000

    corrected directory issue for docs and index.

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,7 +2,7 @@ class DocsController < ApplicationController
     before_action :find_doc, only: [:show, :edit, :update, :destroy]
     
     def index
-        @docs = Doc.all.order('created_at DESC')
+        @docs = Doc.all.order("created_at DESC")
     end
     
     def show
@@ -34,7 +34,7 @@ class DocsController < ApplicationController
     private
     
     def find_doc
-         @doc = Doc.find(params[:id])
+        @doc = Doc.find(params[:id])
     end
     
     def doc_params
