cd7ca5dcaecb1ea696f81323d713b774e1aae875
weblabs7 <weblabs@live.com>
2017-01-13 18:54:46 +0000

    lecture 27 completed, lost old documents assigned to other users.

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20170113150705) do
 
-  create_table "add_user_id_to_docs", force: :cascade do |t|
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "docs", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
