e830809d84ba110126e862f295960cb84871dce6
weblabs7 <weblabs@live.com>
2017-01-13 20:49:25 +0000

    raked database after adding routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :docs
   authenticated :user do 
-    root "docs#index", as: authenticated_root
+    root "docs#index", as: "authenticated_root"
   end
   root "welcome#index"
 end
